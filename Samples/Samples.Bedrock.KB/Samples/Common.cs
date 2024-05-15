438ab9719e2ab27683cf0e90686b48bb5b5e4a80
Vishwanath Uppala <vishuu@amazon.com>
2024-05-15 18:24:14 +0530

    removed unused usings and code formatting

--- Samples/Samples.Bedrock.KB/Samples/Common.cs
+++ Samples/Samples.Bedrock.KB/Samples/Common.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Samples.Bedrock.KB.Samples
+﻿namespace Samples.Bedrock.KB.Samples
 {
     internal class Common
     {
         public  const string KB_NAME = "my-knowledge-base";
         public const string  KB_DATA_SOURCE_NAME = "my-knowledge-base-data-source";
-
-
-
     }
 }
